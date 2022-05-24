e1e74d6f26560af16bebc8ce6c6894b3638ef513
mehmeteyupoglu-osf <mehmeteyupoglu2@gmail.com>
2022-05-24 09:46:24 +0300

    Add typescript documents

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,33 @@
 "use strict";
 let username = 'Mehmet';
+let numbers = [];
+let arrayOfNumbers = [1, 2, 3];
+var Size;
+(function (Size) {
+    Size["Small"] = "s";
+    Size["Medium"] = "m";
+    Size["Large"] = "l";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+function calculateTax(income) {
+    if (income < 50000)
+        return income * 1.2;
+    return income * 1.3;
+}
+console.log(mySize);
+let employee = {
+    id: 1,
+    name: 'Mehmet',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+function kgToLbs(weight) {
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    }
+    return parseInt(weight) * 2.2;
+}
+console.log(kgToLbs(10));
+console.log(kgToLbs('10kg'));
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,42 @@
 let username: string = 'Mehmet'
+let numbers: any[] = []
+let arrayOfNumbers: number[] = [1, 2, 3]
+
+// PascalCase
+enum Size { Small = 's', Medium = 'm', Large = 'l'}
+
+let mySize: Size = Size.Medium
+
+function calculateTax(income: number): number {
+    if (income < 50000)
+        return income * 1.2
+    return income * 1.3
+}
+
+console.log(mySize)
+
+type Employee = {
+    readonly id: number, 
+    name: string, 
+    retire: (date: Date) => void
+}
+
+let employee: Employee = {
+    id: 1, 
+    name: 'Mehmet', 
+    retire: (date: Date) => {
+        console.log(date)
+    }
+}
+
+function kgToLbs(weight: number | string): number {
+    // Narrowing
+    if(typeof weight === 'number'){
+        return weight * 2.2;
+    }
+
+    return parseInt(weight) * 2.2
+}
+
+console.log(kgToLbs(10));
+console.log(kgToLbs('10kg'));
